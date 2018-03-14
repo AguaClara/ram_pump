4fe7db049519d87a5d01bbd53588fd0bb5f3c7ed
mailemccann <mm2498@cornell.edu>
2018-03-14 16:29:17 -0400

    Slight changes

--- Minimum_Spring_Force.py
+++ Minimum_Spring_Force.py
@@ -4,16 +4,20 @@
 
 # h_drive = height of drive pipe
 # A_drive = Area of drive pipe
-# x_max = maximum spring compression distance
+# x_max = maximum spring compression distance, equal to plate ampltitude
 # h_eff = Necessary pumping height
 # A_eff = Area of effluent flow
 
 # Minimum force balance
 # k_min = minimum k value needed to open from closed position
-k_min=rho*g*h_drive*A_drive/x_max
+h_drive = .51 m
+A_drive = (pi*(.995/2)^2)*.0254
+k_min = rho*g*h_drive*A_drive/x_max
 
 # Equation for k value
 # k = EA/L
 # L_opt = optimal spring length
+# A_spring = Area of spring
 
-# Spring force = rho*g*h*Adrive = E*Ax/L
+# Spring force = rho*g*h_drive*A_drive = E*A_spring*x/L
+L_opt = E*A_spring*x/(rho*g*h_drive*A_drive)
